400eabc761ccb34b0af3005dd0ee9646c846da96
somethingGit <jtse1130@gmail.com>
2023-11-09 01:19:33 -0600

    Improved Footer

--- Tourism-Project-SvelteKit/src/routes/+page.svelte
+++ Tourism-Project-SvelteKit/src/routes/+page.svelte
@@ -1,79 +1,56 @@
-<!--
 <script>
-    import svelteLogo from './assets/svelte.svg'
-    import viteLogo from '/vite.svg'
-    import Counter from './lib/Counter.svelte'
-</script>
--->
-<script>
-    import "../../src/app.css";
+  import "../../src/app.css";
 </script>
   
-  <svelte:head>
-    <title>Niagara Falls Tourism</title>
-    <meta name="description" content="A English project based upon the city Niagara Falls focused on the tourism related to Niagara Falls">
-  </svelte:head>
+<style>
   
-  <header class="fade-in">
-    <div class="grid grid-cols-5 grid-rows-1 gap-10 no-underline text-black my-12 place-items-center">
-      <div class="hover:underline">
-        <a href="/" class="hover:no-underline">History</a>
+</style>
+
+<svelte:head>
+  <title>Niagara Falls Tourism</title>
+  <meta name="description" content="A English project based upon the city Niagara Falls focused on the tourism related to Niagara Falls">
+</svelte:head>
+  
+<body>
+  <header class="fade-in text-black">
+    <div class="flex justify-end">
+      <div class="grid sm:grid-rows-1 sm:grid-cols-4 grid-cols-1 grid-rows-4 gap-5 no-underline text-black my-12 place-items-center shadow-sm w-9/12">
+        <div class="sm:h-0 h-10">
+          <span class="parent flex items-center space-x-4">
+            <a href="/" class="">Niagara Falls Information</a>
+            <a href="/" class="text-black child ">^</a>
+          </span>
+        </div>
+
+        <div class="sm:h-0 h-10">
+          <span class="parent flex items-center space-x-4">
+            <a href="/">Visitor Information</a>
+            <a href="/" class="text-black child ">^</a>
+          </span>
+        </div>
+        
+        <div class="sm:h-0 h-10">
+          <span class="parent flex items-center space-x-4">
+            <a href="/">Your Stay in Niagara Falls</a>
+            <a href="/" class="text-black child ">^</a>
+          </span>
+        </div>
+        
+        <div class="sm:h-0 h-10">
+          <span class="parent flex items-center space-x-4">
+            <a href="/">Work Cited Page</a>
+            <a href="/" class="text-black child">^</a>
+          </span>
+        </div>
+      
       </div>
-      <div class="hover:underline">
-        <a href="/">Attractions</a>
-      </div>
-      <div class="hover:underline">
-        <a href="/">Sports</a>
-      </div>
-      <div class="hover:underline">
-        <a href="/">Your Stay in Niagara Falls</a>
-      </div>
-      <div class="hover:underline"><a href="/">Work Cited Page</a></div>
     </div>
-    <p>Hello World</p>
-  </header>
+  </header> 
+</body>
   
-  <!--
-  <main class ="fade-in">
-    <div>
-      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-        <img src={viteLogo} class="logo" alt="Vite Logo" />
-      </a>
-      <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-        <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-      </a>
-    </div>
-    <h1>Vite + Svelte</h1>
-    <div class=""></div>
-    <div class="card">
-      <Counter />
-    </div>
-  
-    <p>
-      Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-    </p>
-  
-    <p class="read-the-docs">
-      Click on the Vite and Svelte logos to learn more
-    </p>
-  </main>
-  -->
-  
-  <style>
-    .logo {
-      height: 6em;
-      padding: 1.5em;
-      will-change: filter;
-      transition: filter 300ms;
-    }
-    .logo:hover {
-      filter: drop-shadow(0 0 2em #646cffaa);
-    }
-    .logo.svelte:hover {
-      filter: drop-shadow(0 0 2em #ff3e00aa);
-    }
-    .read-the-docs {
-      color: #888;
-    }
-    </style>
-  
+<main>
+  <div class="crop">
+    <img src="/IMG_2397.jpeg" alt="" class="h-1/3 w-full">
+  </div>
+</main>
+
